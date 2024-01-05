379dad833da6d1495ed4ff7af78e68fc8fe2849e
zhangh-cs <zhangh-cs@glodon.com>
2024-01-05 16:43:51 +0800

    feat: enable Versioning

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // snapshot: true,
+  });
+  // or "app.enableVersioning()"
+  app.enableVersioning({
+    type: VersioningType.URI,
   });
   await app.listen(7791);
 }
