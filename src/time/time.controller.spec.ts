458413f1d2d03a587c6229b41d94d1191af86a36
zhangh-cs <zhangh-cs@glodon.com>
2024-01-05 14:48:18 +0800

    test: fix time.controller

--- src/time/time.controller.spec.ts
+++ src/time/time.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TimeController } from './time.controller';
+import { TimeService } from './time.service';
 
 describe('TimeController', () => {
   let controller: TimeController;
@@ -7,6 +8,14 @@ describe('TimeController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TimeController],
+      providers: [
+        {
+          provide: TimeService,
+          useClass: class {
+            // Mock ConfigService methods if needed for testing
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<TimeController>(TimeController);
