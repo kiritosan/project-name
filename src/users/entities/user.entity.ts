56a11531230084ae03edce807cec764eb0861288
zhangh-cs <zhangh-cs@glodon.com>
2024-01-05 17:01:18 +0800

    refactor: make id from number to string

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,8 +3,8 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
   firstName: string;
