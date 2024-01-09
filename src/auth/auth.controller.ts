995e64e38aca8d34bcffae8459eaab52f7c88b1f
zhangh-cs <zhangh-cs@glodon.com>
2024-01-09 16:57:13 +0800

    fix: make login don't need to auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,18 +10,20 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+import { Public } from 'src/common/decorators/public/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 
-  @UseGuards(AuthGuard)
+  // @UseGuards(AuthGuard) // make auth to global, so don't need to declare explicit
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
